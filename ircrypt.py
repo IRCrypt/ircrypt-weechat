0e2e15f6680b9e53274d6c8b18b2f45e4a92e733
Lars Kiesow <lkiesow@uos.de>
2013-12-09 00:32:19 +0100

    Added license

--- ircrypt.py
+++ ircrypt.py
@@ -1,10 +1,49 @@
-# DON'T USE THIS SCRIPT IN THE CURRENT STATE!
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013
+#    Lars Kiesow   <lkiesow@uos.de>
+#    Sven Haardiek <sven@haardiek.de>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and documentation
+# are those of the authors and should not be interpreted as representing
+# official policies, either expressed or implied, of everyone working on
+# this project.
+#
+#
+# Suggestions, Bugs, ...?
+# https://github.com/IRCrypt/ircrypt-weechat
+#
+
 
 SCRIPT_NAME    = 'IRCrypt'
 SCRIPT_AUTHOR  = 'Sven Haardiek <sven@haardiek.de>, Lars Kiesow <lkiesow@uos.de>'
-SCRIPT_VERSION = '0.0.0'
-SCRIPT_LICENSE = 'GPL3'
-SCRIPT_DESC    = 'IRCrypt - blabla'
+SCRIPT_VERSION = '0.1'
+SCRIPT_LICENSE = 'FreeBSD License'
+SCRIPT_DESC    = 'IRCrypt: Encryption layer for IRC'
 
 import weechat, string, os, subprocess, base64
 import time
