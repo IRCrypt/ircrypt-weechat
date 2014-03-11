958e6df794443f233a8b24690c348e452457becd
Sven Haardiek <Sven@Haardiek.de>
2014-03-11 13:21:28 +0100

    Release of Version 0.3
    New features:
    - Only the symmetric encytion is implemented
    - Automatic detection of gpg version
    - Minor features and bugfixes
    
    Signed-off-by: Sven Haardiek <Sven@Haardiek.de>

--- ircrypt.py
+++ ircrypt.py
@@ -58,7 +58,7 @@
 
 SCRIPT_NAME    = 'IRCrypt'
 SCRIPT_AUTHOR  = 'Sven Haardiek <sven@haardiek.de>, Lars Kiesow <lkiesow@uos.de>'
-SCRIPT_VERSION = 'SNAPSHOT'
+SCRIPT_VERSION = '0.2'
 SCRIPT_LICENSE = 'FreeBSD License'
 SCRIPT_DESC    = 'IRCrypt: Encryption layer for IRC'
 
