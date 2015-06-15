IRCrypt/ircrypt-weechat

commit fdb6d7f3f5624a36a6e05999519542c268576ab9

Fixed #63, Remove Control Character \n From Decrypted Messages
By adding \n characters to the message, an attacker with access to the
channel password can inject arbitrary faked server messages (like MODE,
KICK, PRIVMSG) into the client.

Example:

   good looking message\n
   :op MODE #channel +o user

This commit fixes that problem by removing all encrypted \n and \r
characters.

Signed-off-by: Lars Kiesow <lkiesow@uos.de>

ircrypt.py
@@ -147,6 +147,7 @@ def ircrypt_gnupg(stdin, *args):
 			[gnupg, '--batch',  '--no-tty'] + list(args),
 			stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err = p.communicate(stdin)
+
 	return (p.returncode, out, err)
 
 
@@ -260,6 +261,9 @@ def ircrypt_decrypt_hook(data, msgtype, server, args):
 		return args
 	if err:
 		ircrypt_warn(err.decode('utf-8'))
+
+	# We expect IRC commands. Hence there should never be more than one line
+	out = out.replace('\r', ' ').replace('\n', ' ')
 
 	return pre + out.decode('utf-8')
 

commit ea03c8395619b0a434ab52ebd42a9ae835f2097d

Fix Python 3 Problem Introduced With fdb6d7f
Commit fdb6d7f mixed strings and byte arrays. Something Python 3 does
not seem to like.

Signed-off-by: Lars Kiesow <lkiesow@uos.de>

ircrypt.py
@@ -147,7 +147,6 @@ def ircrypt_gnupg(stdin, *args):
 			[gnupg, '--batch',  '--no-tty'] + list(args),
 			stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err = p.communicate(stdin)
-
 	return (p.returncode, out, err)
 
 
@@ -263,7 +262,7 @@ def ircrypt_decrypt_hook(data, msgtype, server, args):
 		ircrypt_warn(err.decode('utf-8'))
 
 	# We expect IRC commands. Hence there should never be more than one line
-	out = out.replace('\r', ' ').replace('\n', ' ')
+	out = out.replace(b'\r', b' ').replace(b'\n', b' ')
 
 	return pre + out.decode('utf-8')
 